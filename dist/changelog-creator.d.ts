8c836a8d2c633bb7b8f73f5f8e5578904805ce79
Mohitesh Aeron <mohitesh@okcredit.in>
2021-05-06 14:50:41 +0530

    Use production executable js instead of main

---
+++ dist/changelog-creator.d.ts
@@ -0,0 +1,10 @@
+type Nullable<T> = T | null | undefined
+export namespace kotlinx.atomicfu {
+    function atomic$ref$<T>(initial: T, trace: kotlinx.atomicfu.TraceBase): kotlinx.atomicfu.AtomicRef<T>;
+    function atomic$boolean$(initial: boolean, trace: kotlinx.atomicfu.TraceBase): kotlinx.atomicfu.AtomicBoolean;
+    function atomic$int$(initial: number, trace: kotlinx.atomicfu.TraceBase): kotlinx.atomicfu.AtomicInt;
+    function atomic$long$(initial: kotlin.Long, trace: kotlinx.atomicfu.TraceBase): kotlinx.atomicfu.AtomicLong;
+}
+export namespace io.ktor.util {
+    function AttributesJsFn(concurrent: boolean): io.ktor.util.Attributes;
+}
