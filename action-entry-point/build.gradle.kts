c065113656bf9d9e278ce72012fb66eb2633517c
Mohitesh Aeron <mohitesh@okcredit.in>
2021-05-07 10:59:55 +0530

    Add new task to copy generated file at project root

--- action-entry-point/build.gradle.kts
+++ action-entry-point/build.gradle.kts
@@ -2,3 +2,15 @@
 dependencies {
     api(project(":action-github-logic"))
 }
+
+tasks.register<Copy>("CopyGeneratedJSToDistribution") {
+    val file = File("${rootDir}/distributions")
+    if(!file.exists()) file.mkdir()
+
+    from("${rootDir}/action-entry-point/build/distributions")
+    into("${rootDir}/distributions")
+}
+
+tasks.named("assemble") {
+    finalizedBy("CopyGeneratedJSToDistribution")
+}
