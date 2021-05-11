24c317e3027b8ad02adb02b196af02d293407511
Mohitesh Aeron <mohitesh@okcredit.in>
2021-05-11 11:44:57 +0530

    Rename to changelog builder for readability

--- action-github-logic/src/main/kotlin/ChangelogBuilder.kt
+++ action-github-logic/src/main/kotlin/ChangelogBuilder.kt
@@ -1,16 +1,28 @@
 import data.GithubClient
-import kotlinx.datetime.Clock
 import models.Inputs
+import models.MilestoneInfo
+import models.PullRequestInfo
 import usecase.FetchLastClosedMilestone
 import usecase.FetchMilestoneByName
 import usecase.BuildChangelog
 import usecase.GetPRsForMilestone
-import utils.actions.info
 import utils.actions.setOutput
 
-class ReleaseNoteBuilder(private val inputs: Inputs) {
+/**
+ * Central builder which generates the changelog from the inputs provided.
+ * 1. First fetches the milestone from title provided or resolving from closed milestones.
+ * 2. Fetches all the PRs using [GetPRsForMilestone]
+ * 3. Build changelog from list of PRs [BuildChangelog]
+ */
+class ChangelogBuilder(private val inputs: Inputs) {
 
-    private val githubClient: GithubClient by lazy { GithubClient(inputs.token, inputs.owner, inputs.repo) }
+    private val githubClient: GithubClient by lazy {
+        GithubClient(
+            inputs.token,
+            inputs.owner,
+            inputs.repo
+        )
+    }
 
     private lateinit var fetchMilestoneByName: FetchMilestoneByName
     private lateinit var fetchLastClosedMilestone: FetchLastClosedMilestone
@@ -23,17 +35,22 @@ class ReleaseNoteBuilder(private val inputs: Inputs) {
 
         print("ℹ️ Milestone found with name ${milestoneInfo.title}, closed on ${milestoneInfo.closedAt}")
         setOutput("milestone", milestoneInfo.title)
-        val pullRequestList = utils.actions.group("ℹ️ Fetching PRs for milestone ${milestoneInfo.title}") {
-            pRsForMilestone(
-                milestone = milestoneInfo.number,
-                closedPRs = milestoneInfo.closedPRs,
-                milestoneClosedAt = milestoneInfo.closedAt ?: milestoneInfo.updated_at ?: Clock.System.now()
-            )
-        }
+
+        val pullRequestList = fetchPRsForMilestone(milestoneInfo)
 
         println("ℹ️ Retrieved ${pullRequestList.size} merged PRs")
+
         return utils.actions.group("ℹ️ Building changelog") {
             buildChangelog(pullRequestList)
+        }
+    }
+
+    private suspend fun fetchPRsForMilestone(milestoneInfo: MilestoneInfo): List<PullRequestInfo> {
+        return utils.actions.group("ℹ️ Fetching PRs for milestone ${milestoneInfo.title}") {
+            pRsForMilestone(
+                milestoneNumber = milestoneInfo.number,
+                closedPRs = milestoneInfo.closedPRs
+            )
         }
     }
 
